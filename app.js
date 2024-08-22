65dd9e9b13ec172a874f1c42ec72407351b15d97
nazid <nazidk773@gmail.com>
2024-08-22 13:10:52 +0530

    add or remove class

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const btns = document.querySelectorAll(".tab-btn");
 const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click",function(e){
+    const id = e.target.dataset.id;
+
+    if(id){
+        // remove active class from others
+        btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+    }
+});
