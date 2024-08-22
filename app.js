449183b8980dc39102477a0981a1f78535347f31
nazid <nazidk773@gmail.com>
2024-08-22 13:16:38 +0530

    add or remove articles

--- app.js
+++ app.js
@@ -11,5 +11,12 @@ about.addEventListener("click",function(e){
             btn.classList.remove("active");
             e.target.classList.add("active");
         });
+
+        // hide other articles
+        articles.forEach(function(article){
+            article.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
     }
 });
